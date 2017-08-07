463b2c7d266a6d28edcd0a0bf7964045f66f6d06
mlinwood221 <linwoodmatthews221@gmail.com>
2017-08-07 14:33:48 +0200

    Fixed and actually implemented ShopifyMetafield

--- src/RocketCode/Shopify/ShopifyMetafield.php
+++ src/RocketCode/Shopify/ShopifyMetafield.php
@@ -4,13 +4,56 @@ namespace RocketCode\Shopify;
 
 
 class ShopifyMetafield extends ShopifyResource {
-    protected static function getResourceSingularName()
+    public function __construct(ShopifyApiUser $parent, $namespace = null, $key = null, $value = '')
+    {
+        parent::__construct($parent);
+        if ($namespace && $key) {
+            $this->setShopifyProperty('namespace', $namespace);
+            $this->setShopifyProperty('key', $key);
+            $this->setValue($value);
+        }
+    }
+
+    public static function getResourceSingularName()
     {
         return 'metafield';
     }
 
-    protected static function getResourcePluralName()
+    public static function getResourcePluralName()
     {
         return 'metafields';
+    }
+
+    public function getNamespace() {
+        return $this->getShopifyProperty('namespace');
+    }
+
+    public function getKey() {
+        return $this->getShopifyProperty('key');
+    }
+
+    public static function buildFullKey($namespace, $key) {
+        return $namespace . '.' . $key;
+    }
+
+    public function getFullKey() {
+        return self::buildFullKey($this->getNamespace(), $this->getKey());
+    }
+
+    public function getValueType() {
+        return $this->getShopifyProperty('value_type');
+    }
+
+    public function getValue() {
+        return $this->getShopifyProperty('value');
+    }
+
+    public function setValue($value) {
+        if (is_integer(($value))) {
+            $this->setShopifyProperty('value_type', 'integer');
+        } else {
+            $this->setShopifyProperty('value_type', 'string');
+        }
+        return $this->setShopifyProperty('value', $value);
     }
 }
